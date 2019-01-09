aecb362985bb900a4d8e8ec5d5d0513c6649886c
CNLAB <CNLAB@DESKTOP-5N8F947>
2019-01-10 01:24:59 +0900

    import 패키지명.클래스명; import 패키지명.*;
    클래스를 포함하고자 하는 경우

---
+++ Test/src/packageTest.java
@@ -0,0 +1,10 @@
+import packageTest.*;
+
+public class packageTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ex1 test1 = new ex1();
+		test1.Print();
+	}
+
+}
---
+++ Test/src/packageTest/ex1.java
@@ -0,0 +1,7 @@
+package packageTest;
+
+public class ex1 {
+	public void Print() {
+		System.out.println("package test");
+	}
+}
